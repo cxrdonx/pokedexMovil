4eb9a8ed46f9f96dd4af1c9d50057bc966e2fd0c
cxrdonx <jose.eduardo.cardona@gmail.com>
2021-04-10 00:47:45 -0600

    ADDING EVOLUTIONS

--- src/app/poke/poke.component.ts
+++ src/app/poke/poke.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules, getUrlScheme } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { AnonymousSubject } from 'rxjs/internal/Subject';
 import {PetitionsService} from '../services/petitions.service';
@@ -13,43 +14,95 @@ export class PokeComponent implements OnInit {
   public pokemon: any;
   public types: any;
   public pokemonId: any;
+  public evolution:any;
+  public evolutions:any;
+  public getEvolutions:any;
+  public getEvolutions2:any;
+  public getEvolutions3:any;
 
   constructor(private _petitionsService: PetitionsService){
-     this.pokemonId = 1;
+     this.pokemonId = 6;
   }
   ngOnInit() {
-    this.getPokemonId();
-    this.getHability();
+   this.getPokemonId();
 
-    this._petitionsService.getTypes().subscribe(
-      result =>{
-        this.types = result;
-  
-      },
-      error =>{
-           console.log(<any>error);
-      }
-    )
-  }
-  getPokemonId(){
-  this._petitionsService.getPokemon(this.pokemonId).subscribe(
+/*
+   this._petitionsService.getPokemon().subscribe(
     result =>{
-       this.pokemon = result;
+      this.pokemon = result;
     },
-   error =>{
-     console.log(<any>error);
+    error =>{
+      console.log(<any>error);
     }
-);
+  )
+  */
+  this._petitionsService.getTypes().subscribe(
+    result =>{
+      this.types = result;
+    },
+    error =>{
+         console.log(<any>error);
+    }
+  );
+
   }
 
-  getHability(){
+    getPokemonId(){
+      this._petitionsService.getPokemon(this.pokemonId).subscribe(
+        result =>{
+           this.pokemon = result;
+        },
+       error =>{
+         console.log(<any>error);
+        }
+    );
     this._petitionsService.getHability(this.pokemonId).subscribe(
       result =>{
-        this.hability = result;
+         this.hability = result;
       },
-      error =>{
-        console.log(<any>error);
+     error =>{
+       console.log(<any>error);
       }
-    );
-  }
+  );
+
+  ////// ACA SE PUEDE COMPACTAR GETURL Y GETHABILITY
+
+  this._petitionsService.getUrl(this.pokemonId).subscribe(
+    result =>{
+      this.evolution = result;
+      this.evolutions = this.evolution.evolution_chain.url;
+       this._petitionsService.getEvolution(this.evolutions).subscribe(
+    result =>{
+      this.getEvolutions = result;
+      this.getEvolutions2 = result;
+      this.getEvolutions3 = result;
+      this.getEvolutions = this.getEvolutions.chain.evolves_to[0].evolves_to[0].species.name;
+      this.getEvolutions2 = this.getEvolutions2.chain.evolves_to[0].species.name;
+      this.getEvolutions3 = this.getEvolutions3.chain.species.name;
+      console.log(this.getEvolutions);
+      console.log(this.getEvolutions2);
+      console.log(this.getEvolutions3);
+ 
+    },
+    error =>{
+      console.log(<any>error);
+    }
+  )
+    },
+    error =>{
+        console.log(<any>error);
+    }
+  )
+ 
 }
+   }
+
+  
+  
+
+
+
+
+
+
+
--- src/app/services/petitions.service.ts
+++ src/app/services/petitions.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LEADING_TRIVIA_CHARS } from '@angular/compiler/src/render3/view/template';
 
 @Injectable()
   export class PetitionsService{
      public url: string;
+     public route: any;
 
       constructor(
           public _http: HttpClient 
@@ -13,11 +15,18 @@ import { Observable } from 'rxjs';
       }
    getPokemon(pokemonId:any): Observable<any> {
           return this._http.get(this.url + 'pokemon/'+pokemonId);
-      }
+   }
     getHability(pokemonId:any): Observable<any>{
         return this._http.get(this.url +'pokemon-species/'+pokemonId);
     }
     getTypes(): Observable<any>{
-        return this._http.get(this.url+'type/50');
+        return this._http.get(this.url + 'type');
+    }
+    getUrl(pokemonId:any): Observable<any>{
+        return this._http.get(this.url+'pokemon-species/'+pokemonId);
+    }
+    getEvolution(evolutions:any): Observable<any>{
+          return this._http.get(evolutions);
+          
     }
 }
